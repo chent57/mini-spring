8c6f920b92a11bfca634cc5f6b8541f7d32c087f
chent57 <chent1993@163.com>
2021-10-11 10:32:16 +0800

    添加bean的注册、实例化和使用流程注释

--- src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
+++ src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
@@ -14,9 +14,16 @@ public class BeanDefinitionAndBeanDefinitionRegistryTest {
 	public void testBeanFactory() throws Exception {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 		BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
+
+		// bean注册时，只是把bean的名字和bean的class类型保存到beanDefinitionMap中
 		beanFactory.registerBeanDefinition("helloService", beanDefinition);
 
+		// bean第一次使用时，才实例化bean，保存在singletonObjects map中
 		HelloService helloService = (HelloService) beanFactory.getBean("helloService");
 		helloService.sayHello();
+
+		// bean非第一次使用，可以直接从singletonObjects map中获取到单例的bean，无需实例化
+		HelloService helloService1 = (HelloService) beanFactory.getBean("helloService");
+		helloService1.sayHello();
 	}
 }
